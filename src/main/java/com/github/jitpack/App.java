f2bbcfe3fe52ac3f76e4e49e8fe2bbeb1870a6d5
rsaavedraf <raul.saavedra.felipe@iais.fraunhofer.de>
2020-09-30 11:12:12 +0200

    Testing updates

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -1,7 +1,7 @@
 package com.github.jitpack;
 
 /**
- * Hello world -- This is the first javadoc line.
+ * Hello world -- first javadoc line! Trailing text after the admiration sign.
  * Changing the description of this class to check updates on the final target 
  * through our javadoc publishing workflow (through Bamboo) for IDS
  *
